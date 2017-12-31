eab19b7f4a48b528a7840664d790a43690228bd7
Sid Haniff <syed_haniff@hotmail.com>
2017-12-31 10:17:19 +0000

    Group goals per game by month and division to demo more complex joins

--- src/main/java/eu/waldonia/labs/flinkball/GoalsByMonth.java
+++ src/main/java/eu/waldonia/labs/flinkball/GoalsByMonth.java
@@ -2,7 +2,7 @@ package eu.waldonia.labs.flinkball;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 
 /**
  * Demonstrates a simple aggregation
@@ -17,21 +17,23 @@ public class GoalsByMonth {
         normalizer = new ResultFileNormalizer();
     }
 
-    public DataSet<Tuple2<Integer,Integer>> goals() throws Exception {
+    public DataSet<Tuple3<Integer,String, Integer>> goals() throws Exception {
 
         DataSet<FootyResult> results = normalizer.normalize();
 
 
-        DataSet<Tuple2<Integer,Integer>> goalsByMonth =
-                results.map(new MapFunction<FootyResult, Tuple2<Integer, Integer>>() {
+        DataSet<Tuple3<Integer,String,Integer>> goalsByMonth =
+                results.map(new MapFunction<FootyResult, Tuple3<Integer, String, Integer>>() {
                     @Override
-                    public Tuple2<Integer, Integer> map(FootyResult footyResult) throws Exception {
-                        return new Tuple2<>(footyResult.month(),
+                    public Tuple3<Integer, String, Integer> map(FootyResult footyResult) throws Exception {
+                        return new Tuple3<>(
+                                footyResult.month(),
+                                footyResult.getDivision(),
                                 footyResult.getFullTimeAwayGoals()+footyResult.getFullTimeHomeGoals());
                     }
                 })
-                        .groupBy(0)
-                        .sum(1)
+                        .groupBy(0,1)
+                        .sum(2)
                 ;
 
         return goalsByMonth;
@@ -41,7 +43,7 @@ public class GoalsByMonth {
     public static void main(String[] args) {
         try {
             GoalsByMonth gols = new GoalsByMonth();
-            DataSet<Tuple2<Integer,Integer>> goalsPerMonth = gols.goals();
+            DataSet<Tuple3<Integer,String,Integer>> goalsPerMonth = gols.goals();
             goalsPerMonth.print();
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/eu/waldonia/labs/flinkball/GoalsByMonthByGame.java
+++ src/main/java/eu/waldonia/labs/flinkball/GoalsByMonthByGame.java
@@ -3,9 +3,10 @@ package eu.waldonia.labs.flinkball;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 
 /**
- * Demonstrates joining two datasets
+ * Demonstrates joining two datasets including how to do a multi-key join
  * @author sih
  */
 public class GoalsByMonthByGame {
@@ -18,44 +19,68 @@ public class GoalsByMonthByGame {
         normalizer = new ResultFileNormalizer();
     }
 
-    public DataSet<Tuple2<Integer, Double>> goalsPerGamePerMonth() throws Exception {
+    /**
+     * This demos a multi-key join between two datasets: goalsPerMonth (per Division) and gamesPerMonth (perDivision)
+     * @return A tuple containing the month, division, and goals per game
+     * @throws Exception
+     */
+    public DataSet<Tuple3<Integer, String, Double>> goalsPerGamePerMonth() throws Exception {
 
+        // clean up the data
         DataSet<FootyResult> results = normalizer.normalize();
 
-
-        DataSet<Tuple2<Integer,Integer>> golsPerMonth =
-                results.map(new MapFunction<FootyResult, Tuple2<Integer, Integer>>() {
+        /*
+            Get the goals per month as per the output in the GoalsByMonth class
+         */
+        DataSet<Tuple3<Integer,String,Integer>> golsPerMonth =
+                results.map(new MapFunction<FootyResult, Tuple3<Integer, String, Integer>>() {
                     @Override
-                    public Tuple2<Integer, Integer> map(FootyResult footyResult) throws Exception {
-                        return new Tuple2<>(footyResult.month(),
+                    public Tuple3<Integer, String, Integer> map(FootyResult footyResult) throws Exception {
+                        return new Tuple3<>(
+                                footyResult.month(),
+                                footyResult.getDivision(),
                                 footyResult.getFullTimeAwayGoals()+footyResult.getFullTimeHomeGoals());
                     }
                 })
-                        .groupBy(0)
-                        .sum(1)
+                        .groupBy(0,1)   // group by month and division
+                        .sum(2)         // sum the number of goals
                 ;
 
-        DataSet<Tuple2<Integer,Integer>> gamesPerMonth =
-                results.map(new MapFunction<FootyResult, Tuple2<Integer, Integer>>() {
+        /*
+            Get the games per month thgouth very similar logic
+         */
+        DataSet<Tuple3<Integer,String,Integer>> gamesPerMonth =
+                results.map(new MapFunction<FootyResult, Tuple3<Integer, String, Integer>>() {
                     @Override
-                    public Tuple2<Integer, Integer> map(FootyResult footyResult) throws Exception {
-                        return new Tuple2<>(footyResult.month(), 1); // every result represents a game
+                    public Tuple3<Integer, String, Integer> map(FootyResult footyResult) throws Exception {
+                        return new Tuple3<>(
+                                footyResult.month(),
+                                footyResult.getDivision(),
+                                1);     // every result represents a game
                     }
                 })
-                        .groupBy(0)
-                        .sum(1)
+                        .groupBy(0,1)   // group by month and division
+                        .sum(2)         // add the number of games
                 ;
 
-        DataSet<Tuple2<Integer, Double>> goalsPerGamePerMatch =
-                golsPerMonth.join(gamesPerMonth).where(0).equalTo(0)    // join the two datasets by month
-                .map(new MapFunction<Tuple2<Tuple2<Integer, Integer>, Tuple2<Integer, Integer>>, Tuple2<Integer, Double>>() {
+        /*
+            Join these two datasets on month and division then divide the goals by the games to give the average goals
+            per game per month per division - a more nuanced result than just the goals per month
+         */
+        DataSet<Tuple3<Integer, String, Double>> goalsPerGamePerMatch =
+                golsPerMonth.join(gamesPerMonth)
+                        .where(0,1).equalTo(0,1)    // join the two datasets by month and division
+                .map(new MapFunction<Tuple2<Tuple3<Integer, String, Integer>, Tuple3<Integer, String, Integer>>, Tuple3<Integer, String, Double>>() {
                     @Override
-                    public Tuple2<Integer, Double> map(Tuple2<Tuple2<Integer, Integer>, Tuple2<Integer, Integer>> joinedDataset) throws Exception {
+                    public Tuple3<Integer, String, Double> map(Tuple2<Tuple3<Integer, String, Integer>, Tuple3<Integer, String, Integer>> joinedDataset) throws Exception {
                         Integer month = joinedDataset.f0.f0;    // get the month from the first dataset
+                        String division = joinedDataset.f0.f1;  // get the division from the first dataset
+
                         // divide the goals per month (f0.f1) by the matches per month (f1.f1)
-                        Double goalsPerGamePerMonth = (joinedDataset.f0.f1.doubleValue()/joinedDataset.f1.f1.doubleValue());
+                        Double goalsPerGamePerMonth = (joinedDataset.f0.f2.doubleValue()/joinedDataset.f1.f2.doubleValue());
                         Double roundedGoalsPerGamePerMonth = Math.round(goalsPerGamePerMonth * 100D)/100D;
-                        return new Tuple2<>(month,roundedGoalsPerGamePerMonth);
+
+                        return new Tuple3<>(month,division,roundedGoalsPerGamePerMonth);
                     }
                 });
 
@@ -66,7 +91,7 @@ public class GoalsByMonthByGame {
     public static void main(String[] args) {
         try {
             GoalsByMonthByGame gols = new GoalsByMonthByGame();
-            DataSet<Tuple2<Integer,Double>> goalsPerMonthPerGame = gols.goalsPerGamePerMonth();
+            DataSet<Tuple3<Integer,String, Double>> goalsPerMonthPerGame = gols.goalsPerGamePerMonth();
             goalsPerMonthPerGame.print();
         } catch (Exception e) {
             e.printStackTrace();
